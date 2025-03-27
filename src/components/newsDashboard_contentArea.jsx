dbe3472f55b0b2a12fb0b2294263f6e11474752b
jicho <jIcho6412@gmail.com>
2025-03-27 23:52:17 +0900

    feat: mockData.json을 불러와 그리드 영역 채우기

--- src/components/newsDashboard_contentArea.jsx
+++ src/components/newsDashboard_contentArea.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
 
 const GridContentArea = styled.div`
   margin-top: 24px;
@@ -9,10 +10,32 @@ const GridContentArea = styled.div`
   border: 1px solid var(--border-default);
 `;
 
+const Cell = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function newsDashboardContentArea() {
+  const [pressData, setPressData] = useState([]);
+
+  useEffect(() => {
+    fetch("/mockData.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setPressData(data);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
   return (
     <>
-      <GridContentArea></GridContentArea>
+      <GridContentArea>
+        {pressData.map((press) => (
+          <Cell key={press.id} className="press">
+            {press.name}
+          </Cell>
+        ))}
+      </GridContentArea>
     </>
   );
 }
