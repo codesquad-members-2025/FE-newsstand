b40112ef7b4bc2cfa48ae8270e4692e71967e010
jicho <jIcho6412@gmail.com>
2025-03-27 21:29:05 +0900

    refactor: newsDashboard_contentArea 컴포넌트 CSS 변경
    Styled-components로 변경

--- src/components/newsDashboard_contentArea.jsx
+++ src/components/newsDashboard_contentArea.jsx
@@ -1,8 +1,18 @@
 import styled from "styled-components";
+
+const GridContentArea = styled.div`
+  margin-top: 24px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  width: 930px;
+  height: 388px;
+  border: 1px solid var(--border-default);
+`;
+
 function newsDashboardContentArea() {
   return (
     <>
-      <div className="viewer"></div>
+      <GridContentArea></GridContentArea>
     </>
   );
 }
