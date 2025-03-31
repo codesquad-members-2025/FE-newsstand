e75f2c61d0a21138ba74d00a7daaa2a094279cf1
jicho <jIcho6412@gmail.com>
2025-03-31 12:20:00 +0900

    feat: activeTab에 따라 displayMode 상태 변화 시키기
    1. 전체 언론사 탭 클릭 시 GridView가 기본으로 활성화되게 한다
    2. 내가 구독한 언론사 탭 클릭 시 ListView가 기본으로 활성화 되게 한다

--- src/components/newsDashboard/NewsDashboard.jsx
+++ src/components/newsDashboard/NewsDashboard.jsx
@@ -1,10 +1,16 @@
 import DashboardHeader from "./header/NewsDashboardHeader.jsx";
 import DashboardContentArea from "./contentArea/NewsDashboardContentArea.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function NewsDashboard() {
   const [activeTab, setActiveTab] = useState("AllPress");
   const [displayMode, setDisplayMode] = useState("GridView");
+
+  useEffect(() => {
+    if (activeTab === "Subscribed") setDisplayMode("ListView");
+    else setDisplayMode("GridView");
+  }, [activeTab]);
+
   return (
     <>
       <DashboardHeader
