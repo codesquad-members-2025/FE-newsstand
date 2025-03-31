376c326575bc359342bf140b848ec9be3cf3a63b
Nago730 <jqk1797@gmail.com>
2025-03-31 19:03:26 +0900

    feat: 현재 날짜를 포맷하여 출력하는 기능 추가
    - getFormattedDate 함수 구현
    - 현재 날짜를 "YYYY. M. D. 요일" 형식으로 반환
    - 요일을 한글로 표시

--- src/components/MainLayout/Header/TodaysDate/index.tsx
+++ src/components/MainLayout/Header/TodaysDate/index.tsx
@@ -1,9 +1,20 @@
 import styled from '@emotion/styled'
 
 function TodaysDate() {
-  const date = '2025. 3. 27 목요일'
+  return <Date>{getFormattedDate()}</Date>
+}
 
-  return <Date>{date}</Date>
+function getFormattedDate(): string {
+  const date: globalThis.Date = new globalThis.Date()
+
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  const weekdays = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일']
+  const weekday = weekdays[date.getDay()]
+
+  return `${year}. ${month}. ${day}. ${weekday}`
 }
 
 const Date = styled.div`
