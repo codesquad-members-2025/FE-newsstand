a24bcca8a000a2a8086ca7943e3c71905b5658da
youngmin <dudals2963@tukrorea.ac.kr>
2025-04-06 23:26:01 +0900

    wip: 오토롤링바 무한 스크롤 기능 구현 완료... 양쪽 시간 딜레이 구현 예정

--- src/components/AutoNewsBarContainer/LeftNewsWrapper.jsx
+++ src/components/AutoNewsBarContainer/LeftNewsWrapper.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import NewsItem from "./NewsItem";
+import { useState, useRef, useEffect } from "react";
+import { timeDelay } from "./rollingController";
 
 const StyledDiv = styled.div`
   display: flex;
@@ -13,9 +15,51 @@ function makeNewsItem(newsData) {
 }
 
 export default function LeftNewsWrapper({ newsData }) {
+  const newsRef = useRef();
+  const [curNews, setcurNews] = useState(0);
+
+  function moveNewxtNews() {
+    setcurNews((prev) => (prev + 1) % 5);
+  }
+
+  const newsSet =
+    curNews === newsData.length - 1
+      ? [newsData[curNews], newsData[0]]
+      : newsData.slice(curNews, curNews + 2);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (!newsRef.current) return;
+  //     setTimeout(() => {
+  //       if (!newsRef.current) return;
+  //       newsRef.current.style.transition = `transform 0.5s ease-out`;
+  //       newsRef.current.style.transform = `translateY(-17px)`;
+  //       // moveNewxtNews();
+  //     }, 5000);
+  //     return () => {
+  //       clearInterval(interval);
+  //     };
+  //   }, 5500);
+  // }, []);
+
+  useEffect(() => {
+    const runInterval = async () => {
+      if (!newsRef.current) return;
+      await timeDelay(1000);
+      newsRef.current.style.transition = `transform 0.5s ease-out`;
+      newsRef.current.style.transform = `translateY(-17px)`;
+      await timeDelay(500);
+      moveNewxtNews();
+      newsRef.current.style.transition = `none`;
+      newsRef.current.style.transform = `translateY(0px)`;
+    };
+
+    runInterval();
+  }, [curNews]);
+
   return (
     <>
-      <StyledDiv>{makeNewsItem(newsData)}</StyledDiv>
+      <StyledDiv ref={newsRef}>{makeNewsItem(newsSet)}</StyledDiv>
     </>
   );
 }
