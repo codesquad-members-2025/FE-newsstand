f6bc41068d41505b9f8d5829cfda91a07da0bf66
jicho <jIcho6412@gmail.com>
2025-04-10 18:43:24 +0900

    feat(RollinArea): 무한 롤링 되도록 뉴스 리스트 이어지게끔  구현

--- src/components/rollingArea/RollingArea.jsx
+++ src/components/rollingArea/RollingArea.jsx
@@ -11,9 +11,6 @@ const RollingBox = styled.div`
   width: 461px;
   height: 49px;
   background-color: var(--surface-alt);
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
   border: 1px solid var(--border-default);
   box-sizing: border-box;
   overflow: hidden;
@@ -43,16 +40,22 @@ const PressNewsTitle = styled.div`
   }
 `;
 
-export default function RollingArea() {
-  const newsList = [
-    { press: "연합뉴스", title: "[1보] 김기현·안철수 본경선 진출" },
-    { press: "중앙일보", title: "[속보] 삼성전자 1분기 실적 발표" },
-    { press: "한겨레", title: "서울시, 청년 월세 지원 대상 확대" },
-    { press: "조선일보", title: "美 반도체 법안 통과…한국 영향은?" },
-    { press: "경향신문", title: "기후변화로 여름 더 길어졌다" },
-  ];
+const newsList = [
+  { press: "연합뉴스", title: "[1보] 김기현·안철수 본경선 진출" },
+  { press: "중앙일보", title: "[속보] 삼성전자 1분기 실적 발표" },
+  { press: "한겨레", title: "서울시, 청년 월세 지원 대상 확대" },
+  { press: "조선일보", title: "美 반도체 법안 통과…한국 영향은?" },
+  { press: "경향신문", title: "기후변화로 여름 더 길어졌다" },
+];
 
+function maskExtendedList(list) {
+  return [...list, list[0]];
+}
+
+export default function RollingArea() {
+  const extendedNewsList = maskExtendedList(newsList);
   const [index, setIndex] = useState(0);
+  const [isTransition, setIsTransition] = useState(true);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -62,15 +65,27 @@ export default function RollingArea() {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    if (index === extendedNewsList.length - 1) {
+      setTimeout(() => {
+        setIsTransition(false);
+        setIndex(0);
+      }, 500);
+      setTimeout(() => {
+        setIsTransition(true);
+      }, 510);
+    }
+  }, [index]);
+
   const getRollingStyle = {
     transform: `translateY(-${index * 49}px)`,
-    transition: "transform 0.5s ease-in-out",
+    transition: isTransition ? "transform 0.5s ease-in-out" : "none",
   };
   return (
     <RollingContainer>
       <RollingBox>
         <RollingList style={getRollingStyle}>
-          {newsList.map((news, i) => (
+          {extendedNewsList.map((news, i) => (
             <RollingItem key={i}>
               <PressName>{news.press}</PressName>
               <PressNewsTitle as="a">{news.title}</PressNewsTitle>
@@ -80,7 +95,7 @@ export default function RollingArea() {
       </RollingBox>
       <RollingBox>
         <RollingList style={getRollingStyle}>
-          {newsList.map((news, i) => (
+          {extendedNewsList.map((news, i) => (
             <RollingItem key={i}>
               <PressName>{news.press}</PressName>
               <PressNewsTitle as="a">{news.title}</PressNewsTitle>
